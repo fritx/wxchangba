06ee7ffa6c99d1e9bbe46cd1c7fce82a145f6cd3
Fritz Lin <uxfritz@163.com>
2017-02-01 16:35:02 +0800

    歌曲长度展示时 不超过60″

--- src/server/lib/fillsong.js
+++ src/server/lib/fillsong.js
@@ -5,11 +5,15 @@ module.exports = function fillsong(song){
   song.timestr = song.createtime || '-'
   song.timestr1 = datestr.tostr1(song.createtime) || '-'
   song.timestr2 = datestr.tostr2(song.createtime) || '-'
-  if (song.playlength == null) {
+
+  var len = song.playlength
+  if (len == null) {
     song.lengthstr = '-'
   } else {
-    song.lengthstr = song.playlength + '″'
+    len = Math.min(len, 60) // 不应该显示成超过60″
+    song.lengthstr = len + '″'
   }
+
   if (!song.localfile) song.localfile = song.mediaid + '.mp3'
   song.filepath = 'media/' + song.localfile
   if (!song.title) {
